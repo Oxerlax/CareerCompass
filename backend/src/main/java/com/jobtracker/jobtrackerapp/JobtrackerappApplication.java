8e170889038fac46b2dad25c348d0dde62db44e3
Kris <kob20800@gmail.com>
2026-05-12 02:09:21 -0400

    Initialization of Spring Boot backend

---
+++ backend/src/main/java/com/jobtracker/jobtrackerapp/JobtrackerappApplication.java
@@ -0,0 +1,13 @@
+package com.jobtracker.jobtrackerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrackerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobtrackerappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jobtracker/jobtrackerapp/JobtrackerappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobtracker.jobtrackerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackerappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
