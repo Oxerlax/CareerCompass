4191d2e3ab57ec3f37bb3b9df3af4bfe0714bb0d
Kris <kob20800@gmail.com>
2026-05-19 19:50:43 -0400

    Implemented rough structure of User entity, will expand to use DTOs for security

--- backend/src/main/java/com/jobtracker/models/entities/User.java
+++ backend/src/main/java/com/jobtracker/models/entities/User.java
@@ -1,5 +1,41 @@
 package com.jobtracker.models.entities;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+    @NotBlank
+    private String displayName;
+    @NotBlank
+    private String passwordHash;
+    private String profession;
+    @NotNull
+    private Instant createdAt;
+    @NotNull
+    private Instant updatedAt;
+    @NotNull
+    private Instant lastLoginAt;
+    @OneToMany
+    private List<JobApplication> jobApplications;
+    @OneToMany
+    private List<Company> companies;
+    @OneToMany
+    private List<Contact> contacts;
+    @OneToMany
+    private List<Document> documents;
 }
