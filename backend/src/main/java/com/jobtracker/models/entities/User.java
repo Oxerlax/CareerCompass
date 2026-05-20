0c75168ea95fe4a32b8dea5960e77fe0fd848e32
Kris <kob20800@gmail.com>
2026-05-19 20:12:29 -0400

    Ensure User is an Entity in our database as a table

--- backend/src/main/java/com/jobtracker/models/entities/User.java
+++ backend/src/main/java/com/jobtracker/models/entities/User.java
@@ -5,13 +5,21 @@ import java.util.List;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
